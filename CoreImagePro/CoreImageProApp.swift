470bb950ec802160a8529b046de011ba62218c86
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-10-29 11:32:28 +0600

    project init

---
+++ CoreImagePro/CoreImageProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageProApp.swift
+//  CoreImagePro
+//
+//  Created by Shah Md Imran Hossain on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
